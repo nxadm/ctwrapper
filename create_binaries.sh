58f6ec58d8dfac92c8f87e04296fe9ef8bf13b75
Claudio Ramirez <pub.claudio@gmail.com>
2018-06-12 11:37:42 +0200

    debug travis

--- create_binaries.sh
+++ create_binaries.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 set -xv
-APP=`basename $(pwd)`
+APP=ctwrapper
 BIN_DIR=/var/tmp/$APP
-BIN="$BIN_DIR/$APP"
+BASE_NAME="$BIN_DIR/$APP"
 PLATFORMS=("windows/amd64" "windows/386" "darwin/amd64" "darwin/386" "linux/amd64" "linux/386")
 
 function build {
     GOOS=$1
     GOARCH=$2
-    OUTPUT="${BIN}-${GOOS}-${GOARCH}"
+    OUTPUT="${BASE_NAME}-${GOOS}-${GOARCH}"
     if [ $GOOS = "windows" ]; then
         OUTPUT+='.exe'
     fi
