258df7218541d9fdaa8ea955c983708c9093b8b6
Claudio Ramirez <pub.claudio@gmail.com>
2019-07-19 18:24:08 +0200

    Better doc

--- cli.go
+++ cli.go
@@ -53,7 +53,7 @@ Examples:
   $ ctwrapper -r https://github.com/nxadm/ctwrapper.git -d /var/tmp/project \           
     -s "secret/production/third-party/repo-password"                            
   $ ctwrapper -r https://github.com/nxadm/ctwrapper.git -u foo -p bar \           
-    -d /project	 -- -vault-addr 'https://10.5.32.5:8200 -exec /sbin/my-server          
+    -d /project	-- -vault-addr 'https://10.5.32.5:8200 -exec /sbin/my-server          
 
 `
 
